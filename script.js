2346f3c984777a81954560a5c0a01759dc420212
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-04-04 11:12:53 -0400

    added api url

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-import {apiKey} from '...keys'
+const apiUrl = `https://api.api-ninjas.com/`
+
+fetch( apiUrl, {
+    method: 'GET',
+    headers: {
+        'content-type': 'application/json',
+        'X-Api-Key': apiKey
+    }
+})
+.then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Network response was not ok.');
+  })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error('There was a problem fetching the data:', error);
+  });
