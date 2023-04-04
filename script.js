fa2d691d1a8157901213629df041d680f4c3d735
Warren Zhang <warrenzhang17@gmail.com>
2023-04-04 11:19:48 -0400

    consts

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const apiUrl = `https://api.api-ninjas.com/`
+const apiAbdominals = `https://api.api-ninjas.com/v1/exercises?muscle=abdominals`
+const apiAbductors = `https://api.api-ninjas.com/v1/exercises?muscle=abductors`
+const apiAdductors = `https://api.api-ninjas.com/v1/exercises?muscle=adductors`
+const apiBiceps = `https://api.api-ninjas.com/v1/exercises?muscle=biceps`
+const apiCalves = `https://api.api-ninjas.com/v1/exercises?muscle=calves`
+const apiChest = `https://api.api-ninjas.com/v1/exercises?muscle=chest`
+const apiForearms = `https://api.api-ninjas.com/v1/exercises?muscle=forearms`
+const apiGlutes = `https://api.api-ninjas.com/v1/exercises?muscle=glutes`
+const apiHamstrings = `https://api.api-ninjas.com/v1/exercises?muscle=hamstrings`
+const apiLats = `https://api.api-ninjas.com/v1/exercises?muscle=lats`
+const apiLowerBack = `https://api.api-ninjas.com/v1/exercises?muscle=lower_back`
+const apiMiddleBack = `https://api.api-ninjas.com/v1/exercises?muscle=middle_back`
+const apiNeck = `https://api.api-ninjas.com/v1/exercises?muscle=neck`
+const apiQuadriceps = `https://api.api-ninjas.com/v1/exercises?muscle=quadriceps`
+const apiTraps = `https://api.api-ninjas.com/v1/exercises?muscle=traps`
+const apiTriceps = `https://api.api-ninjas.com/v1/exercises?muscle=triceps`
+
 
 fetch( apiUrl, {
     method: 'GET',
